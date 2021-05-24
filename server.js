fa394375bab529c4a6bcfe581554bebe895b6891
Roshan Kumar Ojha <roshanboyrko@gmail.com>
2021-05-24 16:59:14 +0530

    fix findbyID delete

--- server.js
+++ server.js
@@ -48,24 +48,59 @@ app.get('/blogs', (req, res) => {
     })
 } )
 
-app.get("/:id", (req, res) => {
-    Blogs.findById(req.params.id)
-    .then((data) => {
-      if (!data) {
-        return res.status(404).send({
-          message: "Data not found with id " + req.params.id
-        });
-      }
-      res.status(200).send(data);
-      console.log(data);
-    })
-    .catch((err) => {
-        return res.status(500).send({
-          message: "Error retrieving data with id " + req.params.id,
-        });
-      }) 
+// app.get("/:id", (req, res) => {
+//     Blogs.findById(req.params.id)
+//     .then((data) => {
+//       if (!data) {
+//         return res.status(404).send({
+//           message: "Data not found with id " + req.params.id
+//         });
+//       }
+//       res.status(200).send(data);
+//       console.log(data);
+//     })
+//     .catch((err) => {
+//         return res.status(500).send({
+//           message: "Error retrieving data with id " + req.params.id,
+//         });
+//       }) 
     
-});
+// });
+app.get("/blogs/:id", (req, res) => {
+    console.log(req.params.id);
+    Blogs.findById(req.params.id)
+      .then((data) => {
+        if (!data) {
+          return res.status(404).send({
+            message: "Data not found with id " + req.params.id
+          });
+        }
+        res.status(200).send(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        return res.status(500).send({
+          message: "Error retrieving data with id " + req.params.id
+        });
+      });
+  });
+
+  app.delete("/blogs/:id", (req, res) => {
+    Blogs.findByIdAndRemove(req.params.id)
+      .then((data) => {
+        if (!data) {
+          return res.status(404).send({
+            message: "Blog not found "
+          });
+        }
+        res.send({ message: "Blog deleted successfully!" });
+      })
+      .catch((err) => {
+        return res.status(500).send({
+          message: "Could not delete user "
+        });
+      });
+  });
 
 // Listner 
 app.listen(port, ()=> console.log(`listening to localhost: ${port} `));
